1a4c1ef022a52c1ef490cc031e5fa7c1cfa27032
Emily Perry <emluperry@gmail.com>
2022-11-04 13:56:57 +0000

    Walking and simple falling.
    Issues may arise in future with stairs and falling next to a wall or off of a platform.

--- GEP-Lemmings/Assets/Scripts/Lemming_Movement.cs
+++ GEP-Lemmings/Assets/Scripts/Lemming_Movement.cs
@@ -6,10 +6,11 @@ public class Lemming_Movement : MonoBehaviour
 {
     private Rigidbody m_RB;
 
+    [Header("Lemming Properties")]
     [SerializeField] private float m_Speed = 5;
-    [SerializeField] private float m_MaxSpeed = 50;
 
     private Vector3 m_direction = new Vector3(1, 0, 0);
+    private bool m_isFalling = false;
 
     private void Start()
     {
@@ -18,16 +19,27 @@ public class Lemming_Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        m_RB.AddForce(m_direction * (m_Speed * Time.deltaTime), ForceMode.Force);
+        if(!m_isFalling)
+        {
+            Vector2 NeededAcceleration = (m_Speed * m_direction - new Vector3(m_RB.velocity.x, 0, 0)) / Time.fixedDeltaTime;
 
-        //f = m*a
-        //a = v/t
-        //v = s/t
+            m_RB.AddForce(NeededAcceleration, ForceMode.Force);
+        }
+        if(m_RB.velocity.y < 0)
+        {
+            m_isFalling = true;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        m_direction *= -1;
-        
+        if (!m_isFalling)
+        {
+            m_direction *= -1;
+        }
+        else
+        {
+            m_isFalling = false;
+        }
     }
 }
