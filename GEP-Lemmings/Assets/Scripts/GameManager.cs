3c2d7f2d04c4ec157aaedfd08540bae39df79de4
Emily Perry <emluperry@gmail.com>
2022-11-11 14:33:26 +0000

    Object Pool Lemmings

--- GEP-Lemmings/Assets/Scripts/GameManager.cs
+++ GEP-Lemmings/Assets/Scripts/GameManager.cs
@@ -7,19 +7,31 @@ public class GameManager : MonoBehaviour
     [Header("Level Properties")]
     [SerializeField] private GameObject m_LemmingObject;
     [SerializeField][Min(0f)] private int m_MaxLemmings = 10;
-    private List<GameObject> m_ArrLemmings = new List<GameObject>();
+    private GameObject[] m_ArrLemmings;
+    private int m_LastActiveLemming = 0;
     [SerializeField][Min(0.01f)] private float m_LemmingSpawnDelay = 0.5f;
     private float m_CurrentInterval = 0;
     [SerializeField] private GameObject m_LevelSpawnPoint;
     [SerializeField] private GameObject m_LevelEndPoint;
 
+    private void Start()
+    {
+        m_ArrLemmings = new GameObject[m_MaxLemmings];
+        for(int index = 0; index < m_MaxLemmings; index++)
+        {
+            m_ArrLemmings[index] = Instantiate(m_LemmingObject, m_LevelSpawnPoint.transform.position, Quaternion.identity, gameObject.transform);
+            m_ArrLemmings[index].SetActive(false);
+        }
+        m_CurrentInterval = m_LemmingSpawnDelay;
+    }
+
     private void Update()
     {
         m_CurrentInterval += Time.deltaTime;
-        if(m_CurrentInterval >= m_LemmingSpawnDelay && m_ArrLemmings.Count < m_MaxLemmings)
+        if(m_CurrentInterval >= m_LemmingSpawnDelay && m_LastActiveLemming < m_MaxLemmings)
         {
-            GameObject Lemming = Instantiate(m_LemmingObject, m_LevelSpawnPoint.transform.position, Quaternion.identity, gameObject.transform);
-            m_ArrLemmings.Add(Lemming);
+            m_ArrLemmings[m_LastActiveLemming].SetActive(true);
+            m_LastActiveLemming++;
             m_CurrentInterval = 0;
         }
     }
