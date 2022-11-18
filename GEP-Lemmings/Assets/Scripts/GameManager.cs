bba7cbe3ddc2e72cb8e0b33a976764627d57a3a2
Emily Perry <emluperry@gmail.com>
2022-11-18 14:54:43 +0000

    First UI Steps
    Required to select role to assign.

--- GEP-Lemmings/Assets/Scripts/GameManager.cs
+++ GEP-Lemmings/Assets/Scripts/GameManager.cs
@@ -5,22 +5,29 @@ using System;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Level Properties")]
     [SerializeField] private GameObject m_LemmingObject;
+
+    [Header("Level Properties")]
     [SerializeField][Min(0f)] private int m_MaxLemmings = 10;
     private GameObject[] m_ArrLemmings;
     private int m_LastActiveLemming = 0;
+
     [SerializeField][Min(0.01f)] private float m_LemmingSpawnDelay = 0.5f;
     private float m_CurrentInterval = 0;
+
+    [Header("In-Scene References")]
     [SerializeField] private GameObject m_LevelSpawnPoint;
     [SerializeField] private Exit_Object m_LevelEndPoint;
 
-    private void Start()
+    [Header("UI References")]
+    [SerializeField] private UIManager m_UIHandler;
+
+    private void Awake()
     {
         m_LevelEndPoint.onLemmingExit += DeactivateLemming;
 
         m_ArrLemmings = new GameObject[m_MaxLemmings];
-        for(int index = 0; index < m_MaxLemmings; index++)
+        for (int index = 0; index < m_MaxLemmings; index++)
         {
             m_ArrLemmings[index] = Instantiate(m_LemmingObject, m_LevelSpawnPoint.transform.position, Quaternion.identity, gameObject.transform);
             m_ArrLemmings[index].SetActive(false);
