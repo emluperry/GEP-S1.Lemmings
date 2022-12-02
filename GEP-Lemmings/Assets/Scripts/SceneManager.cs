c5a49da7e4f5f58b5d7129ff4a6ce59ce9bfd4ed
emluperry <emluperry@gmail.com>
2022-12-02 17:12:29 +0000

    Main Menu Created

---
+++ GEP-Lemmings/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField] private Button_OnClick[] m_Buttons;
+
+}
