f39261ce1e76327a6417d4ec5673a06cc12e031c
Emily Perry <emluperry@gmail.com>
2022-12-02 14:59:29 +0000

    Exit sfx

--- GEP-Lemmings/Assets/Scripts/Exit_Object.cs
+++ GEP-Lemmings/Assets/Scripts/Exit_Object.cs
@@ -9,12 +9,20 @@ public class Exit_Object : MonoBehaviour
     [SerializeField] private GameObject m_ParticleEffect;
     [SerializeField] private float m_EffectDuration = 0.5f;
 
+    private AudioSource m_AudioSource;
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Lemming_Movement lemming = other.GetComponent<Lemming_Movement>();
         if(lemming)
         {
             onLemmingExit?.Invoke(lemming.m_LemmingID);
+            m_AudioSource.Play();
             StartCoroutine(StartConfetti());
         }
     }
